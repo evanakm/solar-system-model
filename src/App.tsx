b582d37e9158b33c3c82440e5283d28226b1f9aa
Evan Meikleham <emeikleham@ibm.com>
2022-04-13 00:38:22 -0400

    Uploading working branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Sidebar from './Components/Sidebar';
+import { MVCContextProvider } from './Contexts/MVCContext';
+import SolarSystemViewport from './Components/SolarSystemViewport';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <React.Fragment>
+
+            <div className="wrapper d-flex align-items-stretch">
+                <MVCContextProvider>
+                    <Sidebar/>
+                    <div className="content">
+                        <SolarSystemViewport />
+                    </div>
+                </MVCContextProvider>
+            </div>
+
+        </React.Fragment>
+    );
 }
+
+// <div id="solar-system"></div>
 
 export default App;
